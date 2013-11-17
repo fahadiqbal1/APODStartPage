7d27b3270665c958c5fde9fa7dfaed2e23435b22
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-11-17 16:52:02 -0700

    CSS testing

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,6 @@ function getPic(data) {
     var regex   = /img src=\"([a-zA-Z0-9\_\.\/\:]*)\"/;
     var match = data.match(regex);
     var src = match ? match[1] : '';
-    var css = ' no-repeat center center fixed';
     $('#img').html('<img id="apodImg" src="'+src+'"/>');
-    $('html').css('background-image', 'url(' + src + ')'+css+'');
+    $('html').css('background-image', 'url(' + src + ')');
 }
