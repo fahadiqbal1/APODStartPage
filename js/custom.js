6c6db7ebb3ff4dc8af898709e07394573cb258f7
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-11-17 14:30:10 -0700

    Testing AJAX calls

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,7 @@ function getData() {
         url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.acme.com/jef/apod/rss.xml&num=1",
         dataType: "jsonp",
         success: function (data) {
-            alert(data.responseData.feed.title);
+            alert(data.responseData.feed.entries[0].title);
         }
     });
 }
