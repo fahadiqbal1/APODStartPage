e0aafe17bbdd024df177eb5c545e4db98dc5325f
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-11-17 14:04:36 -0700

    Initial Commit

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ function getData() {
     $.ajax({
         type: "GET",
         url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.acme.com/jef/apod/rss.xml&num=1",
-        dataType: "json",
+        dataType: "jsonp",
         success: function (data) {
             alert(data.responseData.feed.title);
         }
