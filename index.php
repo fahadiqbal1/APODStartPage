cc784875c52d06b3b27eec1a6f660d1beaf22f9c
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-11-17 13:17:20 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include '_config.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $app['name'] ?></title>
+</head>
+<body>
+<div>
+    <h1>Welcome</h1>
+    <p>This is merely a test</p>
+</div>
+</body>
+</html>
