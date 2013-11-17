2acbdfca448a95b5a24ba4a92e61c9351301c72f
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-11-17 15:28:50 -0700

    Testing AJAX calls

--- index.php
+++ index.php
@@ -8,11 +8,30 @@
     <title><?php echo $app['name'] ?></title>
     <script src="js/jquery.js" type="text/javascript"></script>
     <script src="js/custom.js" type="text/javascript"></script>
+    <style type="text/css">
+        #bg {
+            position: fixed;
+            top: -50%;
+            left: -50%;
+            width: 200%;
+            height: 200%;
+        }
+        #bg img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            margin: auto;
+            min-width: 50%;
+            min-height: 50%;
+        }
+    </style>
 </head>
 <body>
 <div>
     <h1 id="header">Welcome</h1>
-    <div id="img"></div>
+    <div id="bg"></div>
     <p>
         <small>
             <?php echo $app['version'] ?>
