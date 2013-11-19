691c3a29b83f3809abfc6f4f566f42f9b3e857fa
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-11-18 22:40:59 -0700

    Adding Side Menu

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
 $(function() {
     console.log( "ready!" );
     getData();
+
+    $("[data-toggle='tooltip']").tooltip();
+    $('#simple-menu').sidr({
+        displace: true
+    });
+    $('#close-menu').sidr({
+        displace: true
+    });
 });
+
 
 function getData() {
     $.ajax({
@@ -9,7 +18,7 @@ function getData() {
         url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.acme.com/jef/apod/rss.xml&num=1",
         dataType: "jsonp",
         success: function (data) {
-            //$('#headertext').html(data.responseData.feed.entries[0].title);
+            $('#headertext').html(data.responseData.feed.entries[0].title);
             getPic(data.responseData.feed.entries[0].content);
         }
     });
