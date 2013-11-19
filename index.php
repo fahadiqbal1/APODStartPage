87c7dcca4f8ff7b126064cdc32de99144c48d0a4
Fahad Iqbal <cfb60309@opayq.com>
2013-11-19 08:55:06 -0700

    Adding features

--- index.php
+++ index.php
@@ -16,19 +16,23 @@
     </head>
     <body>
         <div id="fullscreen_bg" class="fullscreen_bg"/>
-        <a id="simple-menu" href="#sidr" type="button" class="btn btn-default">
+        <a id="simple-menu" href="#sidr" type="button" class="btn btn-default btn-xs" data-toggle="tooltip" data-placement="right" title="Pull Out Menu">
             <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+            <i class="fa fa-bars fa-lg"></i>
         </a>
         <div id="sidr">
             <h1 class="appName"></h1>
+            <ul id="sideBarLinks"></ul>
             <p>
-                <a id="headertext" class="btn btn-xs btn-link" href="#" data-toggle="tooltip" data-placement="bottom" title="first tooltip">Hover over me</a>
+                <a id="headertext" class="btn btn-xs btn-link" data-toggle="tooltip" data-placement="bottom" title="Name of Background"></a>
                 <input type="text" placeholder="Search Google">
-                <a id="close-menu" href="#sidr" class="btn btn-info btn-xs btn-block">Close Menu</a>
+                <a id="close-menu" href="#sidr" class="btn btn-info btn-xs btn-block"><i class="fa fa-sign-out fa-flip-horizontal"></i> Close Menu</a>
             </p>
+        </div>
+        <div id="widgetWindow" class="round">
+            <div id="clockWidget" class="widget">
+                <h1 id="clock"></h1>
+            </div>
         </div>
     </body>
 </html>
--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 $(function() {
     console.log( "ready!" );
-    getData(); getConfig();
+    getData(); getConfig(); getLinks();
+    loadWidgets();
 
     $("[data-toggle='tooltip']").tooltip();
     $('#simple-menu').sidr({
@@ -9,6 +10,12 @@ $(function() {
     $('#close-menu').sidr({
         displace: true
     });
+
+    var start = new Date;
+
+    setInterval(function() {
+        $('.Timer').text((new Date - start) / 1000 + " Seconds");
+    }, 1000);
 });
 
 
@@ -19,6 +26,7 @@ function getData() {
         dataType: "jsonp",
         success: function (data) {
             $('#headertext').html(data.responseData.feed.entries[0].title);
+            $('#headertext').attr('href',data.responseData.feed.entries[0].link);
             getPic(data.responseData.feed.entries[0].content);
         }
     });
@@ -36,3 +44,40 @@ function getConfig() {
         $('.appName').html(data.name);
     });
 }
+
+function getLinks() {
+    $.getJSON( "links.json", function( data ) {
+        $.each(data, function() {
+            console.log('Name: ' + this.name);
+            $('#sideBarLinks').append('<li><a href="'+this.link+'"> <i class="'+this.icon+' fa-border"></i> '+this.name+'</a></li>')
+        });
+    });
+}
+
+function loadWidgets() {
+    clockWidget();
+}
+
+function clockWidget() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+// add a zero in front of numbers<10
+    m=checkTime(m);
+    s=checkTime(s);
+    $('#clock').text(h+":"+m+":"+s);
+    console.log(h+":"+m+":"+s);
+    var t;
+    t = setTimeout(function () {
+        clockWidget()
+    }, 1000);
+    function checkTime(i) {
+        if (i<10)
+        {
+            i="0" + i;
+        }
+        return i;
+    }
+}
+
