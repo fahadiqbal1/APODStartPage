9b75dfb0ac26735c3d0ab3d81d46be277083acd5
Fahad Iqbal <cfb60309@opayq.com>
2013-11-21 14:01:57 -0700

    Adding a space in the name of the background

--- js/custom.js
+++ js/custom.js
@@ -34,7 +34,7 @@ function getData() {
         url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.acme.com/jef/apod/rss.xml&num=1",
         dataType: "jsonp",
         success: function (data) {
-            $('#headertext').html(data.responseData.feed.entries[0].title + '<i class="fa fa-angle-double-right"></i>').attr('href',data.responseData.feed.entries[0].link);
+            $('#headertext').html(data.responseData.feed.entries[0].title + ' <i class="fa fa-angle-double-right"></i>').attr('href',data.responseData.feed.entries[0].link);
             getPic(data.responseData.feed.entries[0].content);
         }
     });
